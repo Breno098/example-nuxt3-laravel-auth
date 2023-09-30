ed01cbbfd779acacc1d38ce08a1842b397d28b1c
Breno <brenohenrique098@gmail.com>
2023-09-30 14:40:54 -0300

    Auth

---
+++ web/layouts/default.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+  import { useAuthStore } from '~/stores/useAuthStore';
+
+  const auth = useAuthStore();
+
+  async function handleLogout() {
+    await auth.logout();
+  }
+</script>
+
+<template>
+  <div>
+    <button
+      v-if="auth.isLoggedIn"
+      @click="handleLogout"
+    >
+      Logout
+    </button>
+
+    <pre>
+      {{ auth.user }}
+    </pre>
+    <ul>
+      <li>
+        <nuxt-link to="/">Home</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/register">Register</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/login">Login</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/auth-only">Auth Only</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/guest-only">Guest Only</nuxt-link>
+      </li>
+    </ul>
+    <slot />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ web/pages/register.vue
@@ -0,0 +1,58 @@
+<script lang="ts" setup>
+  import { useAuthStore } from '~/stores/useAuthStore';
+
+  const form = ref({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: ""
+  });
+
+  const auth = useAuthStore();
+
+  async function handleRegister() {
+    const { error } = await auth.register(form.value)
+
+    if (! error.value) {
+      navigateTo("/");
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="handleRegister">
+      <label>
+        Name 
+        <input type="text" v-model="form.name">
+      </label>
+
+      <br/>
+
+      <label>
+        Email 
+        <input type="email" v-model="form.email">
+      </label>
+
+      <br/>
+
+      <label>
+        Password 
+        <input type="password" v-model="form.password">
+      </label>
+
+      <br/>
+
+      <label>
+        Password Confirmation
+        <input type="password" v-model="form.password_confirmation">
+      </label>
+
+      <button>
+        Register
+      </button>
+    </form>
+  </div>
+</template>
+
+<style scoped></style>
